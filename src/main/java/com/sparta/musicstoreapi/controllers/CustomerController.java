93e003625e90fcd978fca7cd2a7864dc17296913
alex <alex@susanu.com>
2021-12-20 14:37:24 +0000

    added update customer details

--- src/main/java/com/sparta/musicstoreapi/controllers/CustomerController.java
+++ src/main/java/com/sparta/musicstoreapi/controllers/CustomerController.java
@@ -1,8 +1,12 @@
 package com.sparta.musicstoreapi.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sparta.musicstoreapi.entities.Customer;
 import com.sparta.musicstoreapi.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +20,9 @@ public class CustomerController {
 
     @Autowired
     private CustomerRepository customerRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @GetMapping(value = "/chinook/allcustomer")
     public List<Customer> findAllCustomer(){
@@ -34,15 +41,26 @@ public class CustomerController {
         return customerRepository.save(newCustomer);
     }
 
-//    @PutMapping(value = "/chinook/customer/update/{id}")
-//    public Customer updateCustomerById(@PathVariable Integer id, @RequestBody Customer customer){
-//        Optional<Customer> oldCustomer = customerRepository.findById(id);
-//        if(oldCustomer.isEmpty()) return null;
-//        return customerRepository.save(customer);
-//    }
-    @PutMapping(value = "/chinook/customer/{id}")
-    public ResponseEntity<Customer> updateCustomerById(@PathVariable Integer id, @RequestBody Customer newDetails){
+    /**
+     * Update customer by id. If id doesnt exist return no match found
+      * @param id customer id
+     * @param newDetails new customer details as json format
+     * @return no match found if no id
+     */
+    @PutMapping(value = "/chinook/customer/update/{id}")
+    public ResponseEntity<String> updateCustomerById(@PathVariable Integer id, @RequestBody Customer newDetails){
         Optional<Customer> result = customerRepository.findById(id);
-
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("content-type", "application/json; charset=utf-8");
+        if(result.isPresent()){
+            try{
+                ResponseEntity<String> responseEntity =
+                        new ResponseEntity<String>(objectMapper.writeValueAsString(result.get()), headers, HttpStatus.OK);
+                return responseEntity;
+            }catch (JsonProcessingException e){
+                e.printStackTrace();
+            }
+        }
+        return new ResponseEntity<String>("{\"message\": \"no match found\"}", headers, HttpStatus.OK);
     }
 }

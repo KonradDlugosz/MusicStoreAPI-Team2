511e2acbb2678dfa9f8de4b84f31984c6251d06e
alex <alex@susanu.com>
2021-12-20 15:32:09 +0000

    testing

--- src/test/java/com/sparta/musicstoreapi/controllers/CustomerControllerTest.java
+++ src/test/java/com/sparta/musicstoreapi/controllers/CustomerControllerTest.java
@@ -1,13 +1,19 @@
 package com.sparta.musicstoreapi.controllers;
 
+import com.sparta.musicstoreapi.entities.Customer;
 import org.hibernate.annotations.Source;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -19,8 +25,9 @@ class CustomerControllerTest {
 
     @ParameterizedTest
     @ValueSource(strings = {allCustomerUrl})
+    @DisplayName("check status 200 for all customer url")
     void check200Code(String s){
-        HttpResponse httpResponse = null;
+        HttpResponse<String> httpResponse = null;
         HttpClient httpClient = HttpClient.newBuilder().build();
         HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(s)).build();
         try {
@@ -36,7 +43,8 @@ class CustomerControllerTest {
     }
 
     @Test
-    void findCustomerById() {
+    @DisplayName("Check id 1")
+    void findCustomerById() throws IOException, JSONException {
     }
 
     @Test

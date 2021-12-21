a878dddc381a1ae54b9e23422125d19fd1d61e21
EdBencito <45078399+EdBencito@users.noreply.github.com>
2021-12-21 12:40:47 +0000

    Added core functions get & delete

--- src/main/java/com/sparta/musicstoreapi/controllers/AuthController.java
+++ src/main/java/com/sparta/musicstoreapi/controllers/AuthController.java
@@ -8,37 +8,34 @@ import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.crypto.KeyGenerator;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
-public class AuthController
-{
+public class AuthController {
     @Autowired
     private TokenRepository tokenRepository;
 
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    @PostMapping(value = "/token/add")
-    public Token createToken(@RequestParam String email) throws NoSuchAlgorithmException
-    {
-        System.out.println("uwu");
+    @PostMapping(value = "/chinook/token/add")
+    public Token createToken(@RequestParam String email) throws NoSuchAlgorithmException {
         Integer accessLevel = 0;
         Optional<Employee> result = employeeRepository.findByEmail(email);
 
-        if(result.isPresent())
-        {
-            if(result.get().getEmail().contains("Sales"))
-            {
+        if (result.isPresent()) {
+            if (result.get().getTitle().contains("Sales")) {
                 accessLevel = 1;
-            }
-            else
+            } else
                 accessLevel = 2;
         }
 
@@ -49,8 +46,7 @@ public class AuthController
         return token;
     }
 
-    private String generateToken(String email) throws NoSuchAlgorithmException
-    {
+    private String generateToken(String email) throws NoSuchAlgorithmException {
         SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
         KeyGenerator generator = KeyGenerator.getInstance("AES");
         generator.init(128);
@@ -68,5 +64,17 @@ public class AuthController
         builder.setExpiration(exp);
 
         return builder.compact();
+    }
+
+    @PostMapping(value = "/chinook/token/findAll")
+    public List<Token> findAllToken() throws NoSuchAlgorithmException {
+        return tokenRepository.findAll();
+    }
+
+    @PostMapping(value = "/chinook/token/delete/{id}")
+    public ResponseEntity<Integer> deleteTokenById(@PathVariable Integer id) throws NoSuchAlgorithmException {
+          tokenRepository.deleteById(id);
+
+        return new ResponseEntity<>(id, HttpStatus.OK);
     }
 }

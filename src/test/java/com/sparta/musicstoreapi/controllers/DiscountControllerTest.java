be09b92e0673b4a031c19d5c0f5f3d8b8df2a13a
KonradDlugosz <56347935+KonradDlugosz@users.noreply.github.com>
2021-12-21 12:05:16 +0000

    Added Discount for tracks, albums, playlists

---
+++ src/test/java/com/sparta/musicstoreapi/controllers/DiscountControllerTest.java
@@ -0,0 +1,22 @@
+package com.sparta.musicstoreapi.controllers;
+
+public class DiscountControllerTest {
+
+    private static final String GET_ALL_ALBUM_DISCOUNTS = "http://localhost:8080/chinook/album-discount";
+    private static final String GET_ALBUM_DISCOUNT_BY_ID = "http://localhost:8080/chinook/album-discount/1";
+    private static final String POST_ALBUM_DISCOUNT = "http://localhost:8080/chinook/album-discount/add";
+    private static final String PUT_ALBUM_DISCOUNT = "http://localhost:8080/chinook/album-discount/update";
+    private static final String DELETE_ALBUM_DISCOUNT = "http://localhost:8080/chinook/album-discount/delete/1";
+    private static final String GET_ALL_TRACK_DISCOUNTS = "http://localhost:8080/chinook/tracks-discount";
+    private static final String GET_TRACK_DISCOUNT_BY_ID = "http://localhost:8080/chinook/tracks-discount/1";
+    private static final String POST_TRACK_DISCOUNT = "http://localhost:8080/chinook/tracks-discount/add";
+    private static final String PUT_TRACK_DISCOUNT = "http://localhost:8080/chinook/tracks-discount/update";
+    private static final String DELETE_TRACK_DISCOUNT = "http://localhost:8080/chinook/tracks-discount/delete/1";
+    private static final String GET_ALL_PLAYLISTS_DISCOUNTS = "";
+    private static final String GET_PLAYLIST_DISCOUNT_BY_ID = "";
+    private static final String POST_PLAYLIST_DISCOUNT = "";
+    private static final String PUT_PLAYLIST_DISCOUNT = "";
+    private static final String DELETE_PLAYLIST_DISCOUNT = "";
+
+
+}

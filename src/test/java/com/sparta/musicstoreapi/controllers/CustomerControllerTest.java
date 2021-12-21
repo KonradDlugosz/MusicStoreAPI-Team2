b1afc19d9d130d1ab3cc202e9e72d5690f9ff3d2
alex <alex@susanu.com>
2021-12-21 10:08:34 +0000

    testing customer firstname

--- src/test/java/com/sparta/musicstoreapi/controllers/CustomerControllerTest.java
+++ src/test/java/com/sparta/musicstoreapi/controllers/CustomerControllerTest.java
@@ -1,8 +1,7 @@
 package com.sparta.musicstoreapi.controllers;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.json.JSONException;
+import com.sparta.musicstoreapi.entities.Customer;
 import org.junit.Before;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,21 +9,16 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -65,18 +59,15 @@ class CustomerControllerTest {
     }
 
     @Test
-    void findAllCustomer() {
-    }
-
-    @Test
-    void findCustomerById() throws Exception {
-    }
-
-    @Test
-    void addCustomer() {
-    }
-
-    @Test
-    void updateCustomerById() {
+    @DisplayName("Check customer id 1 is Luis")
+    public void checkCustomerIdOneIsLuis(){
+        ObjectMapper objectMapper = new ObjectMapper();
+        Customer customer;
+        try {
+            customer = objectMapper.readValue(new URL("http://localhost:8080/customer/1"), Customer.class);
+            assertEquals("Luis", customer.getFirstName());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }

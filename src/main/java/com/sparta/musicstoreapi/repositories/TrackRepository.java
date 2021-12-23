69e5e1c79d38920a00037bb64371a3a8f0242af3
Ascerted <tanasasilviu@gmail.com>
2021-12-23 11:32:19 +0000

    Fixed requests

--- src/main/java/com/sparta/musicstoreapi/repositories/TrackRepository.java
+++ src/main/java/com/sparta/musicstoreapi/repositories/TrackRepository.java
@@ -4,6 +4,8 @@ import com.sparta.musicstoreapi.entities.Track;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 import java.util.List;
@@ -11,4 +13,9 @@ import java.util.List;
 public interface TrackRepository extends JpaRepository<Track, Integer>
 {
     List<Track> findByNameContains(String name);
+
+    @Query(value = "SELECT * FROM track ", nativeQuery = true)
+    List<Track> getAllByName();
+
+
 }
